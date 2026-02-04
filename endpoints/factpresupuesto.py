caaeabbb9ca309aefab96c0c28035bd7c053413e
Nicolas Taborga <tu_email@ejemplo.com>
2026-02-03 23:05:46 -0400

    Prueba 48

--- endpoints/factpresupuesto.py
+++ endpoints/factpresupuesto.py
@@ -9,7 +9,7 @@ HEADERS = {"Accept": "application/json", "X-Empresa-Codigo": "1"}
 def facpresupuesto():
     fi = request.args.get("FechaInicio")
     ff = request.args.get("FechaFin")
-    url = f"http://34.217.254.222/ReportServerProduccion/api/FacSalvietti/getFacPresupuesto?FechaInicio={fi}&FechaFin={ff}"
+    url = f"http://34.217.254.222/ReportServer/api/FacSalvietti/getFacPresupuesto?FechaInicio={fi}&FechaFin={ff}"
     r = requests.post(url, auth=("alex", "123"), headers=HEADERS)
     return jsonify(r.json())
  
